c862727c9d2713590146227e492d17e7fffa5e9a
Martyn Eggleton <martyn.eggleton@gmail.com>
2013-03-15 23:04:32 +0000

    adds waterbear and minecraft-pi and setups the intial webserver

---
+++ nodejs-server.js
@@ -0,0 +1,76 @@
+//require.paths.unshift(__dirname + '/lib');
+
+var fs = require('fs'),
+    ws = require('ws'),
+    util = require('util'),
+    url = require('url'),
+    http = require('http'),
+    path = require('path'),
+    mime = require('mime');
+
+var httpServer = http.createServer( function(request, response) {
+    var aURL = url.parse(request.url);
+    
+    var pathname = aURL.pathname;
+    //console.log("request =", request);
+    //console.log("pathname =", pathname);
+    
+    if(pathname == "/run")
+    {
+      
+      console.log("aURL =", aURL);
+      
+      /*
+          socket.on('commit-run-file', function(data) {
+      console.log(data);
+      if (data && data.file) {
+        data.file.username = socket.handshake.session.username;
+      }
+
+      exec_helper.execute_program(data.file, false);
+      git_helper.commit_push_and_save(data.file, "Modified " + data.file.name, function(err, status) {
+        socket.emit('commit-file-complete', {message: "Save was successful"});
+      });
+    });
+
+    socket.on('stop-script-execution', function(data) {
+      exec_helper.stop_program(data.file, false);
+    });
+
+
+      
+      */
+      
+      
+    }
+    
+    if (pathname == "/")
+    {
+      pathname = "index.html";
+    }
+    var filename = path.join(process.cwd(), 'waterbear', pathname);
+
+    path.exists(filename, function(exists) {
+        if (!exists) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found");
+            response.end();
+            return;
+        }
+
+        response.writeHead(200, {'Content-Type': mime.lookup(filename)});
+        fs.createReadStream(filename, {
+            'flags': 'r',
+            'encoding': 'binary',
+            'mode': 0666,
+            'bufferSize': 4 * 1024
+        }).addListener("data", function(chunk) {
+            response.write(chunk, 'binary');
+        }).addListener("close",function() {
+            response.end();
+        });
+    });
+});
+
+
+httpServer.listen(8000);

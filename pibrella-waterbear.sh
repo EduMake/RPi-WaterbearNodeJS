f4d8aaa17be17de67c5ef17795c746c627153ed8
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-03-05 20:28:05 +0000

    Adds branch node-pibrella-simple

---
+++ pibrella-waterbear.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+gpio export 27 out
+gpio export 17 out
+gpio export 4 out
+gpio export 22 out
+gpio export 23 out
+gpio export 24 out
+gpio export 25 out
+gpio export 18 out
+
+gpio export 11 in
+gpio export 9 in
+gpio export 7 in
+gpio export 8 in
+gpio export 10 in
+
+gpio -g mode 11 down
+gpio -g mode 9 down
+gpio -g mode 7 down
+gpio -g mode 8 down
+gpio -g mode 10 down
+
+node waterbear.js
